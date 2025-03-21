187c5d8b94971b182a134592a9b317cf739b986f
Hans Ott <hansott@hotmail.be>
2025-03-21 15:57:38 +0100

    Update ULID regex to be case-insensitive for improved matching
    --- FAIL: TestBuildRouteFromURL (0.00s)
        --- FAIL: TestBuildRouteFromURL//posts/01arz3ndektsv4rrffq69g5fav (0.00s)
            utils_test.go:235: expected /posts/:ulid, got /posts/01arz3ndektsv4rrffq69g5fav

--- lib/request-processor/utils/build_route_from_url.go
+++ lib/request-processor/utils/build_route_from_url.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	UUID         = regexp.MustCompile(`(?:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}|00000000-0000-0000-0000-000000000000|ffffffff-ffff-ffff-ffff-ffffffffffff)$`)
-	ULID         = regexp.MustCompile(`^[0-9A-HJKMNP-TV-Z]{26}$`)
+	ULID         = regexp.MustCompile(`(?i)^[0-9A-HJKMNP-TV-Z]{26}$`)
 	OBJECT_ID    = regexp.MustCompile(`^[0-9a-fA-F]{24}$`)
 	NUMBER       = regexp.MustCompile(`^\d+$`)
 	DATE         = regexp.MustCompile(`^\d{4}-\d{2}-\d{2}|\d{2}-\d{2}-\d{4}$`)
