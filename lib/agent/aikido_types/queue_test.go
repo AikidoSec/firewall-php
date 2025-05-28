c5ebb50bffddff84e3079f99486683e7b014f5b8
Hans Ott <hansott@hotmail.be>
2025-05-28 15:52:26 +0200

    Add tests for queues

---
+++ lib/agent/aikido_types/queue_test.go
@@ -0,0 +1,29 @@
+package aikido_types
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewQueue(t *testing.T) {
+	t.Run("it works", func(t *testing.T) {
+		maxSize := 2
+		q := NewQueue[string](maxSize)
+		assert.Nil(t, q.PushAndGetRemovedItemIfMaxExceeded("a"))
+		assert.Nil(t, q.PushAndGetRemovedItemIfMaxExceeded("b"))
+		expected := "a"
+		removedItem := q.PushAndGetRemovedItemIfMaxExceeded("c")
+		assert.NotNil(t, removedItem)
+		assert.Equal(t, expected, *removedItem)
+		assert.Equal(t, maxSize, q.Length())
+		assert.False(t, q.IsEmpty())
+	})
+
+	t.Run("it can clear the queue", func(t *testing.T) {
+		q := NewQueue[string](2)
+		q.PushAndGetRemovedItemIfMaxExceeded("a")
+		q.Clear()
+		assert.Equal(t, 0, q.Length())
+		assert.True(t, q.IsEmpty())
+	})
+}
