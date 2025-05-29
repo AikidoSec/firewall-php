da4e5556e587608f6a718e1467e9f8c7d8de0037
Hans Ott <hansott@hotmail.be>
2025-05-29 13:12:29 +0200

    Add test to verify it sorts on amount of * in wildcard routes

--- tests/server/test_ip_rate_limiting_most_restrictive_wildcard/test.py
+++ tests/server/test_ip_rate_limiting_most_restrictive_wildcard/test.py
@@ -8,6 +8,7 @@ from testlib import *
 2. Sends 5 requests to '/'. Checks that those requests are not blocked.
 3. Send another more 5 request to '/'. Checks that they all are rate limited.
 4. Sends 100 requests to another route '/tests'. Checks that those requests are not blocked.
+5. Tests wildcard route specificity with /api/* vs /api/*/auth/* routes.
 '''
 
 def run_test():
@@ -22,9 +23,20 @@ def run_test():
         assert_response_code_is(response, 429)
         assert_response_header_contains(response, "Content-Type", "text")
         assert_response_body_contains(response, "Rate limit exceeded")
-    
-        
-    
+
+    # /api/v2/auth/login should match both:
+    # - /api/* (50 requests/minute)
+    # - /api/*/auth/* (3 requests/minute)
+    # The more specific /api/*/auth/* should take precedence
+    for i in range(3):
+        response = php_server_get("/api/v2/auth/login")
+        assert_response_code_is(response, 200)
+
+    response = php_server_get("/api/v2/auth/login")
+    assert_response_code_is(response, 429)
+    assert_response_header_contains(response, "Content-Type", "text")
+    assert_response_body_contains(response, "Rate limit exceeded")
+
 if __name__ == "__main__":
     load_test_args()
     run_test()
