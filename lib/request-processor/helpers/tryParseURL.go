6d5e7f4a73032ca1e2fe8b51899aa767b1d35ed4
root <marain@aikido.dev>
2025-08-14 12:47:34 +0000

    add default ports for HTTP and HTTPS schemes

--- lib/request-processor/helpers/tryParseURL.go
+++ lib/request-processor/helpers/tryParseURL.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"net"
 	"net/url"
+	"strconv"
 
 	"golang.org/x/net/idna"
 )
@@ -18,7 +19,17 @@ func TryParseURL(input string) *url.URL {
 	if err == nil {
 		parsedURL.Host = parsedHost
 	}
-
+	// If the port is not present, we need to add it based on the scheme
+	if parsedURL.Port() == "" {
+		port := 0
+		switch parsedURL.Scheme {
+		case "http":
+			port = 80
+		case "https":
+			port = 443
+		}
+		parsedURL.Host = parsedURL.Host + ":" + strconv.Itoa(int(port))
+	}
 	host, port, err := net.SplitHostPort(parsedURL.Host)
 	if err == nil {
 		ip := net.ParseIP(host)
