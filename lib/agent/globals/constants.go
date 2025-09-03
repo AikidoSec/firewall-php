9ddf29918cf70e56fb37e6d8e0211d5e0fca7b2c
Hans Ott <hansott@hotmail.be>
2025-09-03 18:11:27 +0200

    Upgrade Zen internals to 0.1.48 and bump version to 1.3.4

--- lib/agent/globals/constants.go
+++ lib/agent/globals/constants.go
@@ -1,7 +1,7 @@
 package globals
 
 const (
-	Version                             = "1.3.3"
+	Version                             = "1.3.4"
 	ConfigUpdatedAtMethod               = "GET"
 	ConfigUpdatedAtAPI                  = "/config"
 	ConfigAPIMethod                     = "GET"
