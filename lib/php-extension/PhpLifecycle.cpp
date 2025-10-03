8300b7ee387926c8e882b70e66372911577c246c
Tudor TIMCU <tudor@aikido.dev>
2025-10-03 13:47:43 +0300

    f

--- lib/php-extension/PhpLifecycle.cpp
+++ lib/php-extension/PhpLifecycle.cpp
@@ -23,9 +23,10 @@ void PhpLifecycle::RequestShutdown() {
 }
 
 void PhpLifecycle::ModuleShutdown() {
+    char* v = sapi_getenv("SERVER_SOFTWARE", sizeof("SERVER_SOFTWARE")-1)
     if (this->mainPID == getpid()) {
         if (AIKIDO_GLOBAL(sapi_name) == "fpm-fcgi") {
-            AIKIDO_LOG_INFO("Module shutdown called on main PID for php-fpm. Ignoring...\n");
+            AIKIDO_LOG_INFO("Module shutdown called on main PID for php-fpm (server software: %s). Ignoring...\n", v);
             return;
         }
 
@@ -35,7 +36,7 @@ void PhpLifecycle::ModuleShutdown() {
         AIKIDO_GLOBAL(agent).Uninit();
         UnhookAll();
     } else {
-        AIKIDO_LOG_INFO("Module shutdown NOT called on main PID. Uninitializing Aikido Request Processor...\n");
+        AIKIDO_LOG_INFO("Module shutdown NOT called on main PID (server software: %s). Uninitializing Aikido Request Processor...\n", v);
         requestProcessor.Uninit();
     }
 }
