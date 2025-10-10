41006158b5e2b1df85b9a36904f9c4d4e72b1537
Tudor TIMCU <tudor@aikido.dev>
2025-10-10 15:58:14 +0300

    test

--- tests/testlib/testlib.py
+++ tests/testlib/testlib.py
@@ -6,7 +6,8 @@ import os
 import random
 import string
 import datetime
- 
+import re
+
 test_name = ""
 php_port = 0
 mock_port = 0
@@ -25,7 +26,7 @@ def load_test_args():
     mock_port = int(sys.argv[2])
     test_name = sys.argv[3]
     print(f"Loaded test args: test_name={test_name}, php_port={php_port}, mock_port={mock_port}")
-    
+
 def get_mock_port():
     return mock_port
 
@@ -36,29 +37,29 @@ def localhost_get_request(port, route="", headers={}, benchmark=False):
 
     r = s.get(f"http://localhost:{port}{route}", headers=headers)
 
-    end_time = datetime.datetime.now()    
+    end_time = datetime.datetime.now()
     delta = end_time - start_time
     elapsed_ms = delta.total_seconds() * 1000
-    
+
     if benchmark:
         benchmarks.append(elapsed_ms)
-        
+
     time.sleep(0.001)
     return r
 
 def localhost_post_request(port, route, data, headers={}, benchmark=False):
     global benchmarks, s
-    
+
     start_time = datetime.datetime.now()
-    
+
     r = s.post(f"http://localhost:{port}{route}", json=data, headers=headers)
-    end_time = datetime.datetime.now()    
+    end_time = datetime.datetime.now()
     delta = end_time - start_time
     elapsed_ms = delta.total_seconds() * 1000
-    
+
     if benchmark:
         benchmarks.append(elapsed_ms)
-    
+
     time.sleep(0.001)
     return r
 
@@ -108,13 +109,13 @@ def assert_event_contains_subset(event, event_subset, dry_mode=False):
         if dry_mode:
             return False
         raise assertion_error
-    
+
     print(f"Searching {event_subset} in {event} (dry_mode = {dry_mode})...")
 
     if event is None:
         print(f"Event is None!")
         return False
-        
+
     if isinstance(event_subset, dict):
         found_all_keys = True
         for key, value in event_subset.items():
@@ -137,9 +138,9 @@ def assert_event_contains_subset(event, event_subset, dry_mode=False):
     else:
         if event_subset != event:
             return result(AssertionError(f"Value mismatch: {event_subset} != {event}"))
-        
+
     return True
-        
+
 def assert_event_contains_subset_file(event, event_subset_file):
     event_subset = None
     with open(event_subset_file, 'r') as file:
@@ -149,20 +150,23 @@ def assert_event_contains_subset_file(event, event_subset_file):
 
 def assert_started_event_is_valid(event):
     assert_event_contains_subset(event, {"type": "started", "agent": { "library": "firewall-php" } })
-    
+
 def assert_detection_event_is_valid(event):
     assert_event_contains_subset(event, {"type": "detected_attack", "agent": { "library": "firewall-php" } })
-    
+
 def assert_response_code_is(response, status_code):
     assert response.status_code == status_code, f"Status codes are not the same: {response.status_code} vs {status_code}"
-    
+
 def assert_response_header_contains(response, header, value):
     assert header in response.headers, f"Header '{header}' is not part of response headers: {response.headers}"
     assert value in response.headers[header], f"Header '{header}' does not contain '{value}' but '{response.headers[header]}'"
 
 def assert_response_body_contains(response, text):
     assert text in response.text, f"Test '{text}' is not part of response body: {response.text}"
-    
+
+def assert_is_valid_ip(ip_address):
+    assert re.match(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$", ip_address), f"Invalid IP address: {ip_address}"
+
 def mock_server_wait_for_new_events(max_wait_time):
     initial_number_of_events = len(mock_server_get_events())
     while max_wait_time > 0:
@@ -170,7 +174,7 @@ def mock_server_wait_for_new_events(max_wait_time):
             return True
         time.sleep(5)
         max_wait_time -= 5
-        
+
     return False
 
 
@@ -227,7 +231,7 @@ def add_to_hosts_file(hostname, ip):
 
 def mock_server_down():
     mock_server_post("/mock/down", "")
-    
+
 def mock_server_up():
     mock_server_post("/mock/up", "")
 
