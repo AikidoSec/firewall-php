efd1d6f82c9189461978a569e6ead34d070f7b15
Tudor Timcu <tudor@aikido.dev>
2026-02-09 14:59:58 +0200

    Update version to 1.4.14 in documentation and codebase, reflecting new release for Aikido PHP Firewall. (#383)

--- lib/agent/constants/constants.go
+++ lib/agent/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Version                             = "1.4.13"
+	Version                             = "1.4.14"
 	SocketPath                          = "/run/aikido-" + Version + "/aikido-agent.sock"
 	PidPath                             = "/run/aikido-" + Version + "/aikido-agent.pid"
 	ConfigUpdatedAtMethod               = "GET"
