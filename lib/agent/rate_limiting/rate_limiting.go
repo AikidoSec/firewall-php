ec2076f3a0927d91798eb038423a9ea4487bb787
ioaniftimesei <ioan@aikido.dev>
2026-02-11 11:09:55 +0000

    Refactor after review

--- lib/agent/rate_limiting/rate_limiting.go
+++ lib/agent/rate_limiting/rate_limiting.go
@@ -7,11 +7,12 @@ import (
 
 func AdvanceRateLimitingQueues(server *ServerData) {
 	server.RateLimitingMutex.RLock()
+	defer server.RateLimitingMutex.Unlock()
+
 	endpoints := make([]*RateLimitingValue, 0, len(server.RateLimitingMap))
 	for _, endpoint := range server.RateLimitingMap {
 		endpoints = append(endpoints, endpoint)
 	}
-	server.RateLimitingMutex.RUnlock()
 
 	for _, endpoint := range endpoints {
 		endpoint.Mutex.Lock()
@@ -24,6 +25,7 @@ func AdvanceRateLimitingQueues(server *ServerData) {
 
 func Init(server *ServerData) {
 	utils.StartPollingRoutine(server.PollingData.RateLimitingChannel, server.PollingData.RateLimitingTicker, AdvanceRateLimitingQueues, server)
+	AdvanceRateLimitingQueues(server)
 }
 
 func Uninit(server *ServerData) {
