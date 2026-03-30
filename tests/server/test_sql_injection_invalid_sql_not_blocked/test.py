8ea432986d53e278921594aafd0f53842f3eed1e
Popovici Marian <marian@aikido.dev>
2026-03-30 12:57:34 +0000

    fix

--- tests/server/test_sql_injection_invalid_sql_not_blocked/test.py
+++ tests/server/test_sql_injection_invalid_sql_not_blocked/test.py
@@ -11,6 +11,8 @@ def run_test():
     response = php_server_get("/testDetection?id=1+%2F*")
     assert_response_code_is(response, 200)
     assert_response_body_contains(response, "Error: SQLSTATE[HY000]")
+    assert_events_length_is(mock_server_get_events(), 1)  # No attack event is reported.
+    assert_started_event_is_valid(mock_server_get_events()[0])
 
 if __name__ == "__main__":
     load_test_args()
