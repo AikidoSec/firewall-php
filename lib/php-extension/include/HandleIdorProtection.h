21005a1f53bc3ed84ddb0922cfaa7a0795bc6048
Hans Ott <hansott@hotmail.be>
2026-08-21 14:11:44 +0200

    Fix complication on PHP 7.x

--- lib/php-extension/include/HandleIdorProtection.h
+++ lib/php-extension/include/HandleIdorProtection.h
@@ -17,6 +17,10 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_aikido_get_tenant_id, 0, 0, IS_STRING, 1)
 ZEND_END_ARG_INFO()
 
+#if PHP_VERSION_ID >= 80000
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_aikido_without_idor_protection, 0, 1, IS_MIXED, 0)
+#else
+ZEND_BEGIN_ARG_INFO_EX(arginfo_aikido_without_idor_protection, 0, 0, 1)
+#endif
     ZEND_ARG_TYPE_INFO(0, fn, IS_CALLABLE, 0)
 ZEND_END_ARG_INFO()
