8d71fb7e7f226ad07ff320775b2697deb2987b2c
teta2k <teta2k@yahoo.com>
2026-09-04 14:19:11 +0200

    Prepare 1.5.24 release

--- lib/request-processor/globals/globals.go
+++ lib/request-processor/globals/globals.go
@@ -79,6 +79,6 @@ func CreateServer(token string) *ServerData {
 }
 
 const (
-	Version    = "1.5.23"
+	Version    = "1.5.24"
 	SocketPath = "/run/aikido-" + Version + "/aikido-agent.sock"
 )
--- package/rpm/aikido.spec
+++ package/rpm/aikido.spec
@@ -1,5 +1,5 @@
 Name:           aikido-php-firewall
-Version:        1.5.23
+Version:        1.5.24
 Release:        1
 Summary:        Aikido PHP Extension
 License:        GPL
