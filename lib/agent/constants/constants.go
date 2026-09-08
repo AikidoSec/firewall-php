84846161d12659282af658a4f9ce07933c06ff4e
teta2k <teta2k@yahoo.com>
2026-09-08 12:19:07 +0300

    Prepare 1.5.25 release (#480)

--- lib/agent/constants/constants.go
+++ lib/agent/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Version                             = "1.5.24"
+	Version                             = "1.5.25"
 	RunPath                             = "/run/aikido-" + Version
 	SocketPath                          = RunPath + "/aikido-agent.sock"
 	ConfigUpdatedAtMethod               = "GET"
--- package/rpm/aikido.spec
+++ package/rpm/aikido.spec
@@ -1,5 +1,5 @@
 Name:           aikido-php-firewall
-Version:        1.5.24
+Version:        1.5.25
 Release:        1
 Summary:        Aikido PHP Extension
 License:        GPL
