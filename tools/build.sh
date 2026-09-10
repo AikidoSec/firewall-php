7e514d6e89763ddcba9c886b5aa5b12c404160d4
teta2k <teta2k@yahoo.com>
2026-09-10 09:34:14 +0200

    Update Go to 1.26.8 and gRPC to 1.83.2

--- tools/build.sh
+++ tools/build.sh
@@ -25,12 +25,13 @@ cd ..
 protoc --go_out=agent --go-grpc_out=agent ipc.proto
 protoc --go_out=request-processor --go-grpc_out=request-processor ipc.proto
 cd agent
-go get google.golang.org/grpc
+go mod download
+go mod verify
 go test ./...
 go build -ldflags "-s -w" -o ../../build/aikido-agent
 cd ../request-processor
-go get google.golang.org/grpc
-go get github.com/stretchr/testify/assert
+go mod download
+go mod verify
 go test ./...
 go build -ldflags "-s -w" -buildmode=c-shared  -o ../../build/aikido-request-processor.so
 cd ../../build
--- tools/sample_apps_build.sh
+++ tools/sample_apps_build.sh
@@ -31,13 +31,12 @@ cd ..
 protoc --go_out=agent --go-grpc_out=agent ipc.proto
 protoc --go_out=request-processor --go-grpc_out=request-processor ipc.proto
 cd agent
-go get google.golang.org/grpc
-go mod tidy
+go mod download
+go mod verify
 go build -ldflags "-s -w" -o ../../build/aikido-agent
 cd ../request-processor
-go get google.golang.org/grpc
-go get github.com/stretchr/testify/assert
-go mod tidy
+go mod download
+go mod verify
 go build -ldflags "-s -w" -buildmode=c-shared  -o ../../build/aikido-request-processor.so
 cd ../../build
 CXX=g++ CXXFLAGS="-fPIC -g -O2 -I../lib/php-extension/include" LDFLAGS="-lstdc++" ../lib/php-extension/configure
